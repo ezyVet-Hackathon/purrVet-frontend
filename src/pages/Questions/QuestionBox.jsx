ffeb301d7848bb8ed475adad7a3d7315d264cd15
teinnt <phuongthao234567@gmail.com>
2021-05-29 02:29:36 +1200

    Successfully using Carousel

--- src/pages/Questions/QuestionBox.jsx
+++ src/pages/Questions/QuestionBox.jsx
@@ -1,52 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import uuid from 'react-uuid'
 
 import { icons } from '../../utils'
 
-const renderOptions = (options) => {
-  let gridColumn = '-three-col'
-  if (options.lenght / 2 === 0) {
-    gridColumn = '-two-col'
+const renderOptionAnswer = (answer) => (
+  <div className="optionAnswer" key={uuid()}>
+    {answer}
+  </div>
+)
+
+const renderOptionIcon = (icon) => <img src={icon} alt="Option icon" className="optionImage" />
+
+function QuestionBox({ scrollToRef, onNextHandler, onOnboardingUpdate, onGoBackHandler, question, nextQuestion }) {
+  const myRef = useRef(null)
+
+  const renderOptions = (options) => {
+    let gridColumn = '-three-col'
+    if (options.lenght / 2 === 0) {
+      gridColumn = '-two-col'
+    }
+
+    return (
+      <div className={`container${gridColumn}`}>
+        {options.map((option) => {
+          return (
+            <div>
+              <button className="button-icon" type="button" onClick={onOnboardingUpdate}>
+                {renderOptionIcon(option.src)}
+              </button>
+              <button type="button">{renderOptionAnswer(options.answer)}</button>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 
-  const renderOptionAnswer = (answer) => (
-    <div className="optionAnswer" key={uuid()}>
-      {answer}
-    </div>
-  )
-
-  const renderOptionIcon = (icon) => <img src={icon} alt="Option icon" className="optionImage" />
-
-  const onNextPage = (step) => {
-    // if (experience === '') {
-    //   setErrors({ errorMessage: 'Please select your experience' })
-    // } else {
-    //   props.onOnboardingUpdate({
-    //     key: 'experience',
-    //     value: experience,
-    //   })
-    //   props.onNextHandler('technology')
-    // }
-  }
-
-  return (
-    <div className={`container${gridColumn}`}>
-      {options.map((option) => {
-        return (
-          <div>
-            {renderOptionIcon(option.src)}
-            {renderOptionAnswer(options.answer)}
-          </div>
-        )
-      })}
-    </div>
-  )
-}
-
-function QuestionBox({ question }) {
   return (
     <div className="questionBox">
-      <div>{question.step}</div>
+      <div className="question-name">{question.question}</div>
       <img src={icons.questionMark} alt="QuestionMark" />
       {renderOptions(question.options)}
     </div>
