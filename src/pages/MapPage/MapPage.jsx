56d42ffd21c881ea47214f90360971e71b0196e6
kelly-luo <luo.kelly@hotmail.com>
2021-05-28 17:29:08 +1200

    Markers on MapPage reading dataset

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,73 +1,96 @@
-import React, { useState, useEffect } from 'react'
-import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
-import clinicData from './vets.json'
-// import mapStyles from "./mapStyles";
+// import { GoogleMap, Marker, withScriptjs, withGoogleMap, Google } from 'react-google-maps'
+import React, { createRef } from 'react'
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react'
+import vets from './vets.json'
 
-function Map() {
-  const [selectedPark, setSelectedPark] = useState(null)
+// const MapComponent = (props) => {
+//   return (
+//     <GoogleMap
+//       defaultZoom={8}
+//       key
+//       defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//     >
+//       <Marker
+//         position={{ lat: -34.397, lng: 150.644 }}
+//       />
+//     </GoogleMap>
+//   );
+// }
 
-  useEffect(() => {
-    const listener = (e) => {
-      if (e.key === 'Escape') {
-        setSelectedPark(null)
-      }
-    }
-    window.addEventListener('keydown', listener)
+// const MapView = () => {
+//   return (
+//     <div style={{
+//       padding: 30,
+//       textAlign: "center"
+//     }}>
+//       <h1>purrVet Finder</h1>
+//       <MapComponent
+//         isMarkerShown
+//         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKX3VD9qQtp6esG1Xe52s3vT1DAm72W&pov=3.exp&libraries=geometry,drawing,places"
+//         loadingElement={<div style={{ height: `100%` }} />}
+//         containerElement={<div style={{ height: `400px` }} />}
+//         mapElement={<div style={{ height: `100%` }} />}
 
-    return () => {
-      window.removeEventListener('keydown', listener)
-    }
-  }, [])
+//       />
+//     </div>
+//   )
+// }
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+}
+
+const AnotherMap = (props) => {
+  const mapRef = createRef()
+  const { google } = props
+
+  const handleOnClick = (propsEvent, marker) => {
+    const currentMapRef = mapRef.current.map
+    console.log('marker', marker)
+    console.log(currentMapRef.getBounds())
+  }
+
+  const handleDragEnd = (map) => {
+    console.log('drag end', props, map.getBounds())
+  }
+
+  const getFilteredResult = (latRange, lngRange) => {}
 
   return (
-    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 45.4211, lng: -75.6903 }} defaultOptions={{}}>
-      {clinicData.map((clinic) => (
-        <Marker
-          key={clinic.place_id}
-          position={{
-            lat: clinic.location.lat,
-            lng: clinic.location.lng,
-          }}
-          onClick={() => {
-            setSelectedPark(clinic)
-          }}
-          icon={{
-            scaledSize: new window.google.maps.Size(25, 25),
-          }}
-        />
-      ))}
-
-      {/* {selectedPark && (
-        <InfoWindow
-          onCloseClick={() => {
-            setSelectedPark(null);
-          }}
-          position={{
-            lat: selectedPark.geometry.coordinates[1],
-            lng: selectedPark.geometry.coordinates[0]
-          }}
-        >
-          <div>
-            <h2>{selectedPark.properties.NAME}</h2>
-            <p>{selectedPark.properties.DESCRIPTIO}</p>
-          </div>
-        </InfoWindow>
-      )} */}
-    </GoogleMap>
+    <>
+      <h1>Test</h1>
+      <Map
+        google={google}
+        zoom={14}
+        ref={mapRef}
+        style={mapStyles}
+        initialCenter={{
+          lat: -36.849761,
+          lng: 174.7628903,
+        }}
+        onDragend={(propsEvent, map) => {
+          handleDragEnd(props, map)
+        }}
+      >
+        {vets.map((c) => {
+          return (
+            <Marker
+              name={c.name}
+              position={{
+                lat: c.location.lat,
+                lng: c.location.lng,
+              }}
+              onClick={(propsEvent, marker) => handleOnClick(propsEvent, marker)}
+            />
+          )
+        })}
+      </Map>
+    </>
   )
 }
 
-const MapWrapped = withScriptjs(withGoogleMap(Map))
-
-export default function MapPage(props) {
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
-  )
-}
+// export default MapComponent;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCKX3VD9qQtp6esG1Xe52s3vT1DAm72Wpo',
+})(AnotherMap)
